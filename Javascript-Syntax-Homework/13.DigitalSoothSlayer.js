361743c02449ae92c40b4be835d78a13f7e0e5b9
gondars <g0ndars@yahoo.com>
2014-07-15 13:47:03 +0300

    updated Soothsayer

--- Javascript-Syntax-Homework/13.DigitalSoothSlayer.js
+++ Javascript-Syntax-Homework/13.DigitalSoothSlayer.js
@@ -1,3 +1,8 @@
 /**
  * Created by gondarslol on 7/15/2014.
  */
+function soothsayer(work,language,city,car){
+    return "You will work "+work[1]+" years on "+language[3]+".\nYou will live in "+city[4]+" and drive "+car[4];
+}
+console.log(soothsayer([3, 5, 2, 7, 9], ["Java", "Python", "C#", "JavaScript", "Ruby"],
+    ["Silicon Valley","London", "Las Vegas", "Paris", "Sofia"],["BMW","Audi","Lada","Skoda","Opel"]));
