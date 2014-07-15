c6eaec4982ebeb6519d4ff2128349a1a78a5654e
gondars <g0ndars@yahoo.com>
2014-07-15 14:23:06 +0300

    14

---
+++ Javascript-Syntax-Homework/14.CalculateExpression.js
@@ -0,0 +1,9 @@
+function calculate(form){
+    var input = document.getElementById('input');
+    var submit = document.getElementById('confirm');
+    var result = eval(input.value).toString();
+
+    submit.addEventListener('click',function (e){
+        return document.getElementById('result').innerHTML = result  ;
+    })
+}
